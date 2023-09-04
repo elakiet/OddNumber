6b6fa4f15b7c69afa94c9c4d3d24971814441dd6
elakiet <elaki.kavin@gmail.com>
2023-09-04 14:23:16 +0530

    fix new code

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
 import java.util.Scanner;
 
 
-class Main{
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        PrintOdd(11);
+
+
     }
-}
+    static int PrintOdd(int input){
+        if(input==1) {
+            System.out.println(input);
+            return 0;
+        }
+
+        if(input%2!=0){
+            System.out.println(input);
+            PrintOdd(input-2);
+        }
+
+        if(input%2==0) {
+            PrintOdd(input-1);
+        }
+        return 1;
+    }
